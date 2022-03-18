e70f69c125532489fae726e37a7646dd4a5ccd40
Multipixelone <finn@cnwr.net>
2022-03-18 11:44:03 -0700

    add beginning of reactor script

---
+++ simple reactor.lua
@@ -0,0 +1,11 @@
+--define variables
+local ReactorStatus = "OFFLINE"
+
+
+local ChargeOnOffcolor = colors.green
+local FireUpcolor = colors.green
+local powerUpcolor = colors.green
+local ChargeTextcolor = colors.red
+local RectorTextcolor = colors.red
+
+reactor = peripheral.wrap("fissionReactorLogicAdapter_0")
